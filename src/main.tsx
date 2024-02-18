5acf506358caf34b2f02246ef7dd47436ccf024f
kkkqwerasdf123 <kkkqwerasdf123@naver.com>
2024-02-18 11:33:14 +0900

    router test

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Test from "./pages/test/Test.tsx";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/test" element={<Test />}></Route>
+        <Route path="*" element={<App/>}></Route>
+      </Routes>
+
+    </BrowserRouter>
   </React.StrictMode>,
 )
